5ed1821e5ea8771d28e748ba083b9af65040825e
Prakhar314 <prakharjagwani7@gmail.com>
2020-11-22 14:39:49 +0000

    Job queues added

--- backend/model/model.js
+++ backend/model/model.js
@@ -8,13 +8,12 @@ let animes_i_id = JSON.parse(rawdata_i_id);
 let rawdata_id_i = fs.readFileSync(path.resolve(__dirname, "../data/anime_id_to_index.json"));
 let animes_id_i = JSON.parse(rawdata_id_i);
 
-var model,initial_weights_3,initial_weights_11;
+var model, initial_weights_3, initial_weights_11;
 
 (async () => {
-    try
-    {
+    try {
         model = await tf.loadLayersModel('file://model/tfjs_model/model.json');
-        model.compile({optimizer:"adam",loss:"meanSquaredError"});
+        model.compile({ optimizer: "adam", loss: "meanSquaredError" });
         initial_weights_3 = model.layers[3].getWeights();
         initial_weights_11 = model.layers[11].getWeights();
         console.log(model.summary());
@@ -22,16 +21,15 @@ var model,initial_weights_3,initial_weights_11;
         // console.log(model.layers[3]);
         console.log('Model Loaded');
     }
-    catch(error)
-    {
+    catch (error) {
         console.error(error);
     }
 })();
 
 const dsu = (arr1, arr2) => arr1
-  .map((item, index) => [arr2[index], item]) // add the args to sort by
-  .sort(([arg1], [arg2]) => arg2 - arg1) // sort by the args
-  .map(([, item]) => item); 
+    .map((item, index) => [arr2[index], item]) // add the args to sort by
+    .sort(([arg1], [arg2]) => arg2 - arg1) // sort by the args
+    .map(([, item]) => item);
 
 async function reset_weights() {
     await model.layers[3].setWeights(initial_weights_3);
@@ -39,30 +37,44 @@ async function reset_weights() {
     console.log("reset");
 }
 
-async function predict(ids,scores,num) {
+async function predict(job) {
+    ids = job.data.ids;
+    scores = job.data.scores;
+    num = job.data.num;
     reset_weights();
-    
-    var anime_inputs_array = ids.map((i)=>{ return animes_id_i[i]; });
+
+    var anime_inputs_array = ids.map((i) => { return animes_id_i[i]; });
     var anime_inputs = tf.tensor(anime_inputs_array);
-    var user_inputs = tf.tensor(Array.from({length: ids.length}, (v, i) => 0));
+    var user_inputs = tf.tensor(Array.from({ length: ids.length }, (v, i) => 0));
     var user_scores = tf.tensor(scores);
-    await model.fit([anime_inputs,user_inputs],user_scores, {epochs:1000,verbose:0});
-    console.log(await model.evaluate([anime_inputs,user_inputs],user_scores, {verbose:0}).dataSync());
+    await model.fit([anime_inputs, user_inputs], user_scores, {
+        epochs: 1500, verbose: 0, callbacks: {
+            onEpochEnd: async (epoch, logs) => {
+                if (epoch % 100 == 0) {
+                    // console.log(epoch + ':' + logs.loss);
+                    job.progress(epoch / 15 | 0);
+                }
+            }
+        }
+    });
+    console.log(await model.evaluate([anime_inputs, user_inputs], user_scores, { verbose: 0 }).dataSync());
     console.log("trained");
 
-    var all_anime_inputs_array = Array.from({length: 4036}, (v, i) => i);
-    all_anime_inputs_array = all_anime_inputs_array.filter(x => !anime_inputs_array.includes(x) );
+    var all_anime_inputs_array = Array.from({ length: 4036 }, (v, i) => i);
+    all_anime_inputs_array = all_anime_inputs_array.filter(x => !anime_inputs_array.includes(x));
     // console.log(all_anime_inputs_array.length);
     var all_anime_inputs = tf.tensor(all_anime_inputs_array);
-    user_inputs = tf.tensor(Array.from({length: all_anime_inputs_array.length}, (v, i) => 0));
-    var ans = await Array.from(model.predict([all_anime_inputs,user_inputs]).dataSync());
+    user_inputs = tf.tensor(Array.from({ length: all_anime_inputs_array.length }, (v, i) => 0));
+    var ans = await Array.from(model.predict([all_anime_inputs, user_inputs]).dataSync());
     console.log("predicted");
 
-    var ans_sort = dsu(all_anime_inputs_array,ans);
+    var ans_sort = dsu(all_anime_inputs_array, ans);
     console.log("sorted");
-    return  ans_sort.slice(0,num).map((i)=>{
-        return animes_i_id[i]; 
-    });
+    return {
+        "data": ans_sort.slice(0, num).map((i) => {
+            return animes_i_id[i];
+        })
+    };
 }
 
 
--- backend/routes.js
+++ backend/routes.js
@@ -2,6 +2,17 @@
 var { getAnime, getAnimeMin, getSimAnime } = require('./data/anime_data.js');
 var { model, reset_weights, predict } = require('./model/model.js');
 
+let Queue = require('bull');
+// Connect to a local redis intance locally, and the Heroku-provided URL in production
+let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+
+// Create / Connect to a named work queue
+let workQueue = new Queue('work', REDIS_URL);
+workQueue.process(predict);
+// You can listen to global events to get notified when jobs are processed
+workQueue.on('global:completed', (jobId, result) => {
+    console.log(`Job completed with result ${result}`);
+});
 module.exports = function (app) {
     // anime details and recommendations
     app.get('/anime/:id/:num?', function (req, res) {
@@ -9,15 +20,15 @@ module.exports = function (app) {
         if (!num) {
             num = 10;
         }
-        try{
+        try {
             res.json({
                 "details": getAnime(req.params.id),
                 "recommendations": getSimAnime(req.params.id, num)
-            }); 
+            });
         }
-        catch(err){
+        catch (err) {
             res.status(500);
-            res.send({"error":err});
+            res.send({ "error": err });
         }
     });
 
@@ -27,38 +38,58 @@ module.exports = function (app) {
 
     // user recommendations
     app.get('/recommendations/:num?', function (req, res) {
-        req_animes=req.body.animes;
-        req_scores=req.body.scores;
+        req_animes = req.body.animes;
+        req_scores = req.body.scores;
         req_num = req.params.num;
-        if(req_num==null){
-            req_num=10;
+        if (req_num == null) {
+            req_num = 10;
         }
 
         if (req_animes == null || req_animes.length < 10) {
-            res.status(500).send({"error":"Must provide 10 animes"});
+            res.status(500).send({ "error": "Must provide 10 animes" });
             return;
         }
         if (req_scores == null || req_scores.length != req_animes.length) {
-            res.status(500).send({"error":"Must provide same number of scores as number of anime"});
+            res.status(500).send({ "error": "Must provide same number of scores as number of anime" });
             return;
         }
 
-        req.session.animes = {'animes':req_animes,'scores':req_scores};
+        // req.session.animes = {'animes':req_animes,'scores':req_scores};
         // console.log(req.session.animes);
         console.log("recieved " + req_animes.length + " " + req_scores.length);
 
-        predict(req_animes, req_scores, req_num).then((x) => {
-            res.json({
-                "recommendations": x.map((i) => {
-                    return getAnimeMin(i);
-                })
-            });
-            console.log("responded");
+        workQueue.add({
+            ids: req_animes,
+            scores: req_scores,
+            num: req_num,
+        }).then((job) => {
+            req.session.jobId = job.id;
+            res.json({ id: job.id });
         });
-        
     });
 
-     app.get('/recommendations/saved', function (req, res) {
-        res.send(req.session.animes);
-     });
+    // Allows the client to query the state of a background job
+    app.get('/job/', async (req, res) => {
+        let id = req.session.jobId;
+        let job = await workQueue.getJob(id);
+        if (job === null) {
+            res.status(404).send({
+                message: 'No job'
+            });
+        } else {
+            let state = await job.getState();
+            let progress = job._progress;
+            let reason = job.failedReason;
+            let result = job.returnvalue;
+            if (state == 'completed') {
+                result = result.data.map((i) => getAnimeMin(i));
+                job.remove();
+            }
+            res.json({ id, state, progress, reason, result });
+        }
+    });
+
+    //  app.get('/savedrecommendations', function (req, res) {
+    //     res.send(req.session.animes);
+    //  });
 }
